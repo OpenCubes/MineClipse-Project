b0f48ec913a8f4eb272843bbec76a01348de7a0f
Vinz243 <Vinz243@gmail.com>
2013-06-01 10:11:57 +0200

    iniatl

--- org.eclipse.mineclipse.draft/readme.md
+++
@@ -1,1 +0,0 @@
-This just DRAFT, not definitive
--- org.eclipse.mineclipse.draft/src/org/eclipse/mineclipse/draft/views/GlimpseView.java
+++ org.eclipse.mineclipse.draft/src/org/eclipse/mineclipse/draft/views/GlimpseView.java
@@ -1,17 +1,26 @@
 package org.eclipse.mineclipse.draft.views;
 
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.wb.swt.ResourceManager;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class GlimpseView extends ViewPart {
 
 	public static final String ID = "org.eclipse.mineclipse.draft.views.GlimpseView"; //$NON-NLS-1$
+	private Action action;
+	private Action action_1;
+	private Action action_2;
+	private Action action_3;
 
 	public GlimpseView() {
+		setTitleImage(ResourceManager.getPluginImage(
+				"org.eclipse.mineclipse.draft", "icons/preview.gif"));
 	}
 
 	/**
@@ -21,11 +30,12 @@ public class GlimpseView extends ViewPart {
 	 */
 	@Override
 	public void createPartControl(final Composite parent) {
-		Composite container = new Composite(parent, SWT.NONE);
-
-		Composite composite = new Composite(parent, SWT.NONE);
-		composite.setBackgroundImage(ResourceManager.getPluginImage(
-				"org.eclipse.mineclipse.draft", "icons/draft_preview.gif"));
+		{
+			Label label = new Label(parent, SWT.NONE);
+			label.setBackground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
+			label.setImage(ResourceManager.getPluginImage(
+					"org.eclipse.mineclipse.draft", "icons/draft_preview.gif"));
+		}
 
 		createActions();
 		initializeToolBar();
@@ -37,6 +47,31 @@ public class GlimpseView extends ViewPart {
 	 */
 	private void createActions() {
 		// Create the actions
+		{
+			action = new Action("") {
+
+			};
+			action.setImageDescriptor(ResourceManager.getPluginImageDescriptor(
+					"org.eclipse.mineclipse.draft", "icons/move.gif"));
+		}
+		{
+			action_1 = new Action("") {
+
+			};
+			action_1.setImageDescriptor(ResourceManager
+					.getPluginImageDescriptor("org.eclipse.jdt.ui",
+							"/icons/full/elcl16/refresh.gif"));
+		}
+		{
+			action_2 = new Action("") {
+
+			};
+		}
+		{
+			action_3 = new Action("Pick Color") {
+
+			};
+		}
 	}
 
 	/**
@@ -45,6 +80,8 @@ public class GlimpseView extends ViewPart {
 	private void initializeToolBar() {
 		IToolBarManager toolbarManager = getViewSite().getActionBars()
 				.getToolBarManager();
+		toolbarManager.add(action);
+		toolbarManager.add(action_1);
 	}
 
 	/**
@@ -53,6 +90,7 @@ public class GlimpseView extends ViewPart {
 	private void initializeMenu() {
 		IMenuManager menuManager = getViewSite().getActionBars()
 				.getMenuManager();
+		menuManager.add(action_3);
 	}
 
 	@Override
